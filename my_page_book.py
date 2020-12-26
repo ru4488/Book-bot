55a844d77a7b07ef6cb2929b7ec37de41a42ef27
Dormidontov Dmitry <44_88@mail.ru>
2020-12-26 12:31:59 +0300

    temp

---
+++ my_page_book.py
@@ -0,0 +1,113 @@
+import requests
+import random
+import time
+import json
+
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+
+jar = requests.cookies.RequestsCookieJar()
+
+def get_HTML(url):
+    global jar
+    try:
+        result = requests.get(url , headers={'User-Agent': UserAgent().chrome}, cookies=jar)
+        result.encoding = "utf8"
+        jar = result.cookies
+        
+        return result.text
+    except (requests.RequestException , ValueError):
+        print('сетевая ошибка')
+        return False
+
+
+  # поиск названия книги  
+def parse_book_name(review):
+    name_book = review.find("a" , class_="brow-book-name with-cycle").text
+    return name_book
+
+
+# поиск автора
+def parse_book_author(review):
+    author = review.find("a" , class_="brow-book-author")
+    if author is None:
+        return None
+    return author.text
+
+# оценка пользователя
+def parse_book_score(review): 
+    score = review.find("span" , class_="brow-rating marg-right").text
+    return float(score)
+
+def user_name(soup): #имя пользователя
+    return soup.find('span' , class_ = 'header-profile-login').text
+    
+
+# создание массива из словарей    
+def parse_books(html):
+    
+    all_about_book_list = []
+    soup = BeautifulSoup(html , 'html.parser')
+  
+    
+    for review in soup.find_all("div" , class_="brow-data"):    
+        all_about_book_dir = {}
+        
+ 
+
+        all_about_book_dir['user'] = user_name(soup)
+        all_about_book_dir['title'] = parse_book_name(review)
+        all_about_book_dir['artist']  = parse_book_author(review)
+        all_about_book_dir['score'] = parse_book_score(review)
+
+
+        all_about_book_list.append(all_about_book_dir)
+#  может быть несколько одинаковых прочитаных книг (с разными оценками)
+
+
+
+   
+
+    return all_about_book_list
+
+
+def new_page(url):
+    result = get_HTML(url)
+    soup = BeautifulSoup(result , 'html.parser')
+    
+    if soup.find('span' , id="a-list-page-next-") in soup.find_all('span', class_="pagination__page"):
+
+        return result , 0
+    return result , 1
+
+
+
+        
+
+
+
+
+if __name__ == "__main__":
+    url = 'https://www.livelib.ru/reader/TibetanFox/read'
+    
+    numb = 1
+    page = 1
+    all_page = []
+    while page != 0:
+        
+        html, page = new_page(url + '~' + str(numb))
+
+        all_page.extend(parse_books(html))
+        random_numb = random.randint(7 , 30) 
+        time.sleep(random_numb)
+        numb += 1
+        
+        print(url + '~' + str(numb))
+        print(len(all_page))
+        
+
+
+
+
+
---
+++ username_books_score.py
@@ -0,0 +1,139 @@
+import requests
+import random
+import redis
+
+import time
+import json
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+from ratelimit  import *
+
+jar = requests.cookies.RequestsCookieJar()
+
+
+
+def last_page(pages):
+        soup = BeautifulSoup(pages, 'html.parser')
+        info= soup.find("a" , title= "Последняя страница") 
+        last_page_list = info['href'].split('~')  
+        last_page = int(last_page_list[1])
+        return last_page
+
+# random_numb = random.randint(7 , 30) 
+# @sleep_and_retry
+# @limits(calls =1, period  = random_numb)
+def get_HTML(html): #скачивает HTML файл 
+    global jar
+    try:    
+        print(jar)
+        result = requests.get(html , headers={'User-Agent': UserAgent().chrome}, cookies=jar)
+        result.raise_for_status()
+        result.encoding = "utf8"
+        jar = result.cookies
+        return result.text
+
+    except (requests.RequestException , ValueError):
+        print('сетевая ошибка')
+        return False
+
+
+def teg_book(info_list, review):
+    all_about_book_list = []
+    all_about_book_dir = {}
+    score_book = review.find('div' , class_="group-review-rating")
+    score_list = score_book.text.split()  
+   
+    score = score_list[2]
+    all_about_book_dir['score'] = score
+#название книги
+    if 'автора' in info_list:
+        name_book_list = info_list[info_list.index('книгу') + 1 : info_list.index('автора')]
+        name_book = " ".join(name_book_list)   
+        all_about_book_dir['title'] = name_book
+     
+            #нашел проблему на странице, есть места где нет слов "атовров/автора"
+    elif 'авторов' in info_list: 
+            
+        name_book_list = info_list[info_list.index('книгу') + 1 : info_list.index('авторов')]
+        name_book = " ".join(name_book_list)
+            
+        #     all_about_book_dir['title'] =  name_book
+
+# имя автора книги
+    if 'автора' in info_list:
+        name_artist_list =info_list[info_list.index('автора') + 1 :]
+        name_artist = " ".join(name_artist_list)
+        all_about_book_dir['artist'] = name_artist
+            
+
+    elif 'авторов' in info_list:
+        name_artist_list =info_list[info_list.index('авторов') + 1 :]
+        name_artist = " ".join(name_artist_list)
+        how_much_artists = name_artist.split(', ')
+            
+        for artist in range(len(how_much_artists)):
+            all_about_book_dir1 = {}
+            all_about_book_dir1['artist'] =  how_much_artists[artist]
+            all_about_book_dir1['title'] = name_book
+            all_about_book_dir1['score'] = score
+            all_about_book_list.append(all_about_book_dir1)
+                
+                
+    if len(all_about_book_dir) != 0: 
+        all_about_book_list.append(all_about_book_dir)
+        all_about_book_dir = {}
+    
+    return all_about_book_list
+
+    
+def all_about_books(html):
+
+    soup = BeautifulSoup(html , 'html.parser')
+
+    all_about_book_list = []
+    # all_about_book_dir = {}
+    
+
+
+    
+    for review in soup.find_all('div', class_="block-border card-block expert-review"):
+ 
+        info= review.find("div" , class_="group-login-date dont-author")   
+        info_list = info.text.split()
+        
+
+        all_about_book_list.extend(teg_book(info_list, review))
+
+    return all_about_book_list
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+    url = 'https://www.livelib.ru/reader/Fari22/reviews'
+    pages = get_HTML('https://www.livelib.ru/reader/Fari22/reviews~1')
+    
+    last_page = last_page(pages)  
+    all_page_list = []
+    for web_page in range(1 , last_page + 1):
+        all_page_list.append(url + '~' + str(web_page))
+    print(last_page)
+ 
+    new_page = []
+    for i in range(len(all_page_list)):
+        random_numb = random.randint(7 , 30) 
+        html = get_HTML(all_page_list[i])   
+          
+        new_page.extend(all_about_books(html))
+        
+        time.sleep(random_numb)
+    print(new_page)
+    with open("reviews_of_all_users_books.json", "w") as write_file:
+        json.dump(new_page, write_file)
