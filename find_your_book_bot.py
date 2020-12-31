b72397368553afbec868bddeb29852a718edd41d
Dormidontov Dmitry <44_88@mail.ru>
2020-12-31 17:28:26 +0300

    добавил в find your book запоминание Id

---
+++ find_your_book_bot.py
@@ -0,0 +1,25 @@
+import logging
+import settings
+from telegram.ext import Updater , CommandHandler , MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def start(update, context):
+
+    if 'id' not in context.user_data:
+        context.user_data['id'] = update.message.chat.id
+        update.message.reply_text('Привет, дорогой друг! Присылай свою страницу с прочитанными книгами с сайта https://www.livelib.ru/')
+    update.message.reply_text('Я тебя помню')
+    
+    
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", start))    
+    # dp.add_handler(MessageHandler(Filters.text, your_book))
+    mybot.start_polling()
+    mybot.idle()
+    
+if __name__ == "__main__":
+    main()
