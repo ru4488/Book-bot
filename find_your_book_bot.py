d1cb82260b8f0823d699ddb424f900e48c190628
Dormidontov Dmitry <44_88@mail.ru>
2021-01-04 20:21:50 +0300

    поменял, чтоб бот писал: я тебя помню, когда есть данные id в context.userdata

--- find_your_book_bot.py
+++ find_your_book_bot.py
@@ -4,17 +4,23 @@ from telegram.ext import Updater , CommandHandler , MessageHandler, Filters
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
-def greet_user(update, context):
-    print('Вызван /start')
-    update.message.reply_text('Привет, дорогой друг! Присылай свою страницу с прочитанными книгами с сайта https://www.livelib.ru/')
+def start(update, context):
+    if 'id' in context.user_data:
+        update.message.reply_text('Я тебя помню')
+    else:
+        context.user_data['id'] = update.message.chat.id
+        update.message.reply_text('Привет, дорогой друг! Присылай свою страницу с прочитанными книгами с сайта https://www.livelib.ru/')
 
+    
+    
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))    
+    dp.add_handler(CommandHandler("start", start))    
+    # dp.add_handler(MessageHandler(Filters.text, your_book))
     mybot.start_polling()
     mybot.idle()
     
-
-main()
+if __name__ == "__main__":
+    main()
