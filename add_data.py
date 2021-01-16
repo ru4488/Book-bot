e48d7942d458686094a3b38d7d7f277cbde83a4d
Dormidontov Dmitry <44_88@mail.ru>
2021-01-16 13:32:37 +0300

    Удилал лишние буквы и слова в коде пишет ошибку

--- add_data.py
+++ add_data.py
@@ -7,18 +7,18 @@ from sqlalchemy.orm.exc import NoResultFound
 
 def store_books(all_info):
     for row in all_info:
-        if not Books.query.filter(Books.book_livelib_id == row['book_id']).first():    
+        if not Book.query.filter(Book.livelib_id == row['book_id']).first():    
             user = get_or_create_user(row['user'])
 
-            book = Books(
-                book_name=row['title'],
-                book_livelib_id=row['book_id'],
-                book_author=row['artist']
+            book = Book(
+                name=row['title'],
+                livelib_id=row['book_id'],
+                author=row['artist']
             )
             db_session.add(book)
             db_session.commit()
 
-            review = Reviews(
+            review = Review(
                 user_id=user.id,
                 score=row['score'],
                 book_id=book.id
@@ -29,14 +29,14 @@ def store_books(all_info):
         
 
 def get_or_create_user(username):
-    user = Users.query.filter(Users.user_name == username).first()
+    user = User.query.filter(User.name == username).first()
     if not user:
-        user = Users(user_name = username)
+        user = User(name = username)
         db_session.add(user)
         db_session.commit()
     return user
 
 
 if __name__ == "__main__":
-    all_info = all_page_info('https://www.livelib.ru/reader/kupreeva74/read')
+    all_info = all_page_info('https://www.livelib.ru/reader/LushbaughPizzicato/read')
     store_books(all_info)
--- models.py
+++ models.py
@@ -2,28 +2,28 @@ from sqlalchemy import Column, Integer, String , DECIMAL , ForeignKey
 from sqlalchemy.orm import relationship
 from db import Base, engine
 
-class Books(Base):
+class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
-    book_name = Column(String)
-    book_livelib_id = Column(String , unique=True)
-    book_author =  Column(String)
-    book_for_reviews = relationship("Reviews")
+    name = Column(String)
+    livelib_id = Column(String , unique=True)
+    author =  Column(String)
+    reviews = relationship("Reviews")
 
     def __repr__(self):
         return f'<Book {self.book_name} {self.book_livelib_id} {self.book_author}>'
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
     id = Column(Integer , primary_key=True)
     user_name = Column(String , unique=True )
-    user_for_reviews = relationship("Reviews")
+    reviews = relationship("Reviews")
 
     def __repr__(self):
         return f'<User {self.user_name}>'
 
         
-class Reviews(Base):
+class Review(Base):
     __tablename__ = 'reviews'
     id = Column(Integer, primary_key=True)
     book_id = Column(Integer , ForeignKey('books.id'))
