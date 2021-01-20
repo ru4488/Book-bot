630f8c110dac05ffd7c1f28b823c0c0928b081af
Dormidontov Dmitry <44_88@mail.ru>
2021-01-21 00:04:02 +0300

    С помощью in_ выполнил 2 зыдыния из 3

--- get_data.py
+++ get_data.py
@@ -1,16 +1,32 @@
 from models import User, Review, Book
 
 
+"Список оценок книги под Book.id 759 "
+users_with_same_score = []
+user_with_same_score = {}
+all_score = []
+all_users = []
+result = Review.query.filter(Review.book_id.in_([759]))
+for row in result:
+    # print(row.score)
+    if row.score not in all_score:
+        all_score.append(row.score)
+        user_with_same_score[row.score] = "" 
 
-# все пользователи которые поставили 3 книжке '1000330921'
-book = Book.query.filter(Book.livelib_id == '1000330921').first()
-score = Review.query.filter(Review.book_id == book.id)
-for row in score:
-    print(row.score)
-    if row.score == 3:
-        print(row.user)
 
+''' список авторов через оценки '''
+for row in result:   
+    if row.score in all_score:
+        all_users.append(row.user.name)        
+print(all_users)
 
+"словарь пользователи поставившие одинаковую оценку"
+for row in result: 
+    user_with_same_score = {}
+    user_with_same_score[row.score] = row.user.name
+    users_with_same_score.append(user_with_same_score)
+
+print(users_with_same_score)
 
 
 
