ec7ce65872182560e7590ba4f037fd3ae3adfe73
Dormidontov Dmitry <44_88@mail.ru>
2021-01-20 20:30:07 +0300

    добавил функцию в add_data (get_or_create_book)

--- add_data.py
+++ add_data.py
@@ -7,26 +7,19 @@ from sqlalchemy.orm.exc import NoResultFound
 
 def store_books(all_info):
     for row in all_info:
-        if not Book.query.filter(Book.livelib_id == row['book_id']).first():    
-            user = get_or_create_user(row['user'])   
-
-            book = Book(
-                name=row['title'],
-                livelib_id=row['book_id'],
-                author=row['artist']
-            )
-            db_session.add(book)
-            db_session.commit()
+        # if not Book.query.filter(Book.livelib_id == row['book_id']).first():    
+        user = get_or_create_user(row['user'])   
+        book = get_or_create_book(row)
         
-            if Review.query.filter(and_(Review.book_id == book.id , Review.user_id == user.id)).count() == 0:
-                review = Review(
-                    user_id=user.id,
-                    score=row['score'],
-                    book_id=book.id
-                    )
+        if Review.query.filter(and_(Review.book_id == book.id , Review.user_id == user.id)).count() == 0:
+            review = Review(
+                user_id=user.id,
+                score=row['score'],
+                book_id=book.id
+                )
 
-                db_session.add(review)
-                db_session.commit()
+            db_session.add(review)
+            db_session.commit()
                 
 
 def get_or_create_user(username):
@@ -37,15 +30,26 @@ def get_or_create_user(username):
         db_session.commit()
     return user
 
-
-
+def get_or_create_book(row):
+    book = Book.query.filter(Book.livelib_id == row['book_id']).first()
+    if not book:
+        book = Book(
+                name=row['title'],
+                livelib_id=row['book_id'],
+                author=row['artist']
+            )
+        db_session.add(book)
+        db_session.commit()
+    return book
 
 
 if __name__ == "__main__":
-    url = 'https://www.livelib.ru/reader/livjuly/read'
+    url = 'https://www.livelib.ru/reader/LushbaughPizzicato/read'
     all_info = all_page_info(url)
     store_books(all_info)
-
+# 
 # 'https://www.livelib.ru/reader/LushbaughPizzicato/read'
-
+'https://www.livelib.ru/reader/livjuly/read'
 "https://www.livelib.ru/reader/VartanPopov/read"
+
+'https://www.livelib.ru/reader/kira_katz/read'
