6b84875e73db7e920c435b5e41aaa53a1f723837
Dormidontov Dmitry <44_88@mail.ru>
2021-01-20 22:42:04 +0300

    сделал поиск всех пользователей которые поставили оценку 3 определенной книги

--- get_data.py
+++ get_data.py
@@ -1,11 +1,17 @@
 from models import User, Review, Book
 
-all_4 = Review.query.filter(Review.score.in_([0 , 4])).all()
-book_with_score = []
-# for row in all_4:
-# book_with_4 = Book.query.filter(Book.id.in_(all_4))
-    # book_with_score.append(book_with_4) 
-print(all_4)
+
+
+# все пользователи которые поставили 3 книжке '1000330921'
+book = Book.query.filter(Book.livelib_id == '1000330921').first()
+score = Review.query.filter(Review.book_id == book.id)
+for row in score:
+    print(row.score)
+    if row.score == 3:
+        print(row.user)
+
+
+
 
 
 
