150ada9b79d51a51f55032276c2f99a290519f76
Dormidontov Dmitry <44_88@mail.ru>
2021-07-05 21:57:08 +0300

    перенес базу данных на комп, подключил python к ней

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import settings
 
-engine = create_engine('postgres://cxbsnnoh:TyklLzrRQMcP1RFgluIAMu3INmGO2O0L@hattie.db.elephantsql.com:5432/cxbsnnoh')
+# engine = create_engine(settings.password_cxbsnnoh)
 #engine = create_engine('sqlite:///db_bd_bbook_athor.sqlite', echo=True)
-db_session = scoped_session(sessionmaker(bind=engine))
+# db_session = scoped_session(sessionmaker(bind=engine))
 
+engine = create_engine(settings.password_localhost)
+db_session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
